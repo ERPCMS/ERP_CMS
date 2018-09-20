3b3a3a0605fd35b580a797bd40886091d07e21e1
Zeeshan-123 <zeehaider54@gmail.com>
2018-09-20 21:47:29 +0500

    Update Shipper.aspx.cs
    same issue

--- ERP_CMS/Admin/Setup/Shipper/Shipper.aspx.cs
+++ ERP_CMS/Admin/Setup/Shipper/Shipper.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using ERP_CMS.DB_Code;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,45 @@ namespace ERP_CMS.Admin.Setup.Shipper
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            int userID = getCurrentUserID();
+            if (!IsPostBack)
+            {
+                try
+                {
+                    using (ERP_CMSEntities cntx = new ERP_CMSEntities())
+                    {
 
+                        bool isAdmin = (bool)cntx.Users.Where(f => f.UserID == userID).Select(f => f.IsAdmin).FirstOrDefault();
+                        if (!isAdmin)
+                        {
+                            bool haveRights = PortalUtilities.fnHaveRights(userID, Convert.ToInt32(PortalUtilities.MenuLinks.ManageShipper));
+                            if (!haveRights)
+                            {
+                                Response.Redirect("~/Admin/");
+                            }
+                        }
+
+                    }
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            }
+        public int getCurrentUserID()
+        {
+            int userID = 0;
+            if (Session["userID"] != null || !string.IsNullOrEmpty(Session["userID"] as string))
+            {
+                userID = Convert.ToInt32(Session["userID"]);
+            }
+            else
+            {
+                Response.Redirect("~/Login.aspx");
+            }
+            return userID;
         }
     }
 }
